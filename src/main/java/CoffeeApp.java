7f5dfe91b00fb71a86666eecb967b0fede2a92e0
altayucetas <altayucetas@gmail.com>
2023-09-01 23:02:54 +0300

    Yorum satırları eklendi

--- src/main/java/CoffeeApp.java
+++ src/main/java/CoffeeApp.java
@@ -8,18 +8,20 @@ public class CoffeeApp {
         Scanner getInp = new Scanner(System.in);
         CoffeeMachine myMach = new CoffeeMachine();
 
+        // Kullanıcıdan kahve numarası değeri alınır.
         System.out.print("Lütfen içmek istediğiniz kahvenin numarasını giriniz: ");
         int coffeeNum = Integer.parseInt(getInp.nextLine());
 
+        // Alınan değer 10000'den fazla ise accessAPI fonksiyonu çalıştırılır.
+        // accessAPI fonksiyonu çalıştıktan sonra tekrar kullanıcıdan kahve numarası istenir.
         if(coffeeNum > 10000){
             myMach.accessAPI(coffeeNum);
             System.out.print("Lütfen içmek istediğiniz kahvenin numarasını giriniz: ");
             coffeeNum = Integer.parseInt(getInp.nextLine());
         }
 
+        // Alınan değer 10000'den küçük ise istenilen kahve teslim edilir.
         System.out.println(myMach.getCoffeeInfo(coffeeNum));
-
-
 
     }
 }
--- src/main/java/altay/yucetas/application/CoffeeMachine.java
+++ src/main/java/altay/yucetas/application/CoffeeMachine.java
@@ -4,7 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
+/*
+    Kullanının görebileceği ve erişebileceği tek sınıf yapısıdır.
+    Tanımlandığında menüyü ekrana yazdırır. Kullanıcı menüde gördüğü
+    seçenekler arasında bir seçim yapar. Eğer 0'dan küçük veya menüde
+    belirtilen en büyük sayıdan daha büyük bir değer girerse hata mesajı
+    ekrana yazıdırılır.
+*/
 public class CoffeeMachine {
+
     private final CoffeeMenu myMenu = new CoffeeMenu();
     public CoffeeMachine(){
         System.out.println("Merhaba, kahve makinesine hoş geldiniz. Menü ve fiyatlar aşağıda görüldüğü gibidir.");
@@ -14,6 +22,22 @@ public class CoffeeMachine {
     public String printMenu(){
         return myMenu.toString();
     }
+
+
+    /*
+        Görev tanımında yer almayan ve benim olası tehlike ve uyarıları
+        göstermek için eklediğim fonksiyonlardan birisi "accessAPI"dır.
+        Bu fonksiyon, eğer kullanıcı 10000'den fazla bir sayı girerse çalışır.
+        Asıl amacı, kahve makinesini kontrol eden kişilerin, kahve ekleme ve
+        çıkarma işlemleri yapmasında yardımcı olmasıdır. Fonksiyon ilk olarak
+        girilen değeri kontrol eder. Bu değerin sistem tarafından geliştiriciye
+        verilen bir kod olması ve bu koda sahip olmayan kişilerin sisteme herhangi
+        bir müdahalede bulunamaması amaçlanmıştır. Eğer değer "612546903"ya eşit değilse geçerli
+        bir kimlik numarası girilmesi için uyarı verir. Fakat belirtilen değere eşit ise
+        makinenin sahibi kahve ekleyip çıkarabileceği bir ekran çıktısı alır. Bu ekrandaki
+        çıktıyı seçerek kahve ekleyebilir veya çıkarabilir. Kahve ekleyebilmek için kahve ismi,
+        kahve ücreti ve tarif gerekmektedir. Kahve çıkarmak için ise sadece isim yeterlidir.
+    */
 
     public void accessAPI(int devID){
         if(devID == 612546903){
@@ -81,6 +105,15 @@ public class CoffeeMachine {
             System.out.println("Lütfen geçerli bir kullanıcı kimlik numarası giriniz!");
         }
     }
+
+    /*
+        Kullanıcıdan girilen değer alındıktan sonra kahve seçimi bu fonksiyon ile
+        yapılır. Değer geçersiz ise kullanıcıdan geçerli bir değer girmesi
+        istenir. Değer geçerli ise kahvenin hazırlandığı mesajı bastırılır ve
+        2 saniye beklenir. Ardından ise görevde belirtilen çıktı gösterilir.
+        Örn:
+            Americano seçtiniz. Bu içeceğimiz dört doz Hot Water ve bir doz Espresso içermektedir. Afiyet Olsun.
+    */
     public String getCoffeeInfo(int selectedNum){
         StringBuilder machineAnswer = new StringBuilder();
 
@@ -119,6 +152,18 @@ public class CoffeeMachine {
     }
 }
 
+
+/*
+    Bu sınıf tanımlandıktan sonra Coffee sınıfını kullanarak görevde
+    belirtilen 7 adet kahve objesini tanımlar ve "menu" adındaki
+    array listesine ekler. Ayrıca "menuSize" değişkeni ile de
+    menünün uzunluğunu tutar. Temel olarak kullanılan getter'ları da
+    tanımladıktan sonra görevde yer almayan 3 adet fonksiyon eklemek istedim.
+    Bunlar "addCoffee", "deleteCoffee" ve "clearMenu". "addCoffee" ve "deleteCoffee"
+    fonksiyonları adlarından da anlaşılacağı gibi menüye kahve ekleyip silerler.
+    Ayrıca yaptıkları ekleme ve silme işlemine göre de "menuSize" değişkenini
+    düzenlerler. "clearMenu" fonksiyonu ise menüyü tamamen boşaltmak için kullanılır.
+*/
 class CoffeeMenu{
     private final ArrayList<Coffee> menu = new ArrayList<>();
     private int menuSize;
@@ -150,6 +195,13 @@ class CoffeeMenu{
         menuSize++;
     }
 
+    /*
+        Bu sınıfa dışarıdan erişim yoktur ve bu yüzden kahve ekleme fonksiyonu
+        ancak dışarıdan kullanılabilen CoffeMachine sınıfı tarafından kullanılabilir.
+        Fakat o sınıf ise hiçbir zaman overload edilen ve bir Coffee objesini parametre
+        olarak alan aşağıdaki fonksiyonu kullanmayacaktır. Bu fonksiyonu kolayca test
+        yapabilmek için oluşturdum.
+    */
     public void addCoffee(Coffee addedCoffee){
         menu.add(addedCoffee);
         menuSize++;
@@ -168,6 +220,7 @@ class CoffeeMenu{
         }
     }
 
+    // Yukarıda belirttiğim gibi testin kolay olması için overload ettim.
     public void deleteCoffee(Coffee removedCoffee){
         for (Coffee selected: menu) {
             if(removedCoffee.getName().equals(selected.getName())){
@@ -185,6 +238,18 @@ class CoffeeMenu{
         menuSize = 0;
     }
 
+    /*
+        Oluşturulan menüyü görev tanımında yer aldığı gibi
+        bastırabilmek için bu fonksiyon kullanılır.
+        Örn:
+            1. Espresso (20 TL)
+            2. Double Espresso (29 TL)
+            3. Cappuccino (27 TL)
+            4. Caffe Latte (27 TL)
+            5. Mocha (32 TL)
+            6. Americano (25 TL)
+            7. Hot Water (5 TL)
+    */
     @Override
     public String toString() {
         StringBuilder allMenu = new StringBuilder();
@@ -195,14 +260,36 @@ class CoffeeMenu{
     }
 }
 
+
+/*
+    Adından da anlaşılacağı gibi Coffee sınıfı gerekli parametreleri alıp
+    bu parametrelere göre kahve objesi oluşturur. Görev tanımında belirtilen
+    7 kahve, bu sınıfın objesidir.
+*/
 class Coffee{
     private final String name;
     private final int price;
+
+    /*
+        String olarak alınan kahve tarifi, içindekiler ve bunların miktari
+        olmak üzere bir key-value ikilisi içinde tutulur. Bu sayede çıktı
+        alınmak istediğinde kolaylık sağlanır.
+        Örn:
+            {"Hot Water"=5, "Espresso"=3}
+    */
     private final HashMap<String, Integer> recipe = new HashMap<>();
 
     public Coffee(String inpName, int inpPrice, String inpRecipe){
         this.name = inpName;
         this.price = inpPrice;
+
+        /*
+            Alınan String türündeki tarif, alttaki kod parçası yardımı ile
+            ilk olarak bileşenlerine ayrılarak bir array'e kaydedilir.
+            Ardından da bu bileşenler, array'dan teker teker alınarak
+            miktar ve tür olarak ikiye ayrılır. Bundan sonra ise Hashmap'e
+            eklenirler.
+        */
 
         String[] splitted = inpRecipe.split(", ", 0);
 
@@ -215,15 +302,19 @@ class Coffee{
     public String getName(){
         return name;
     }
+
     public int getPrice(){
         return price;
     }
+
     public HashMap<String, Integer> getRecipe(){
         return recipe;
     }
 
+    // Kahve nesnesi, "Nesne_adı (Nesne_fiyatı TL) şeklinde String çıktısı verir.
     @Override
     public String toString() {
         return this.name + " (" + this.price + " TL)";
     }
 }
+
